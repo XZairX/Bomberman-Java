fabf6bb80234c5d1c4209f5d7f262eaab34fe7f7
Riaz Philippe <rp16181@essex.ac.uk>
2019-10-09 21:38:28 +0100

    Added a BlockSoft class. Added conditional IF statements to display BlockSoft objects in their correct positions. Removed the comment in BlockTile which provided a reference for the row formation of the gamespace.

--- src/game/BlockSoft.java
+++ src/game/BlockSoft.java
@@ -6,17 +6,18 @@ import java.awt.Color;
 import static game.Constants.TILE_OUTLINE_COLOUR;
 import static game.Constants.TILE_RADIUS;
 
-public class BlockTile extends GameObject {
-    private static final Color TILE_COLOUR = Color.GREEN;
+public class BlockSoft extends GameObject {
+    private static final Color TILE_COLOUR = Color.RED;
 
-    public BlockTile(int x, int y, double radius) {
+    public BlockSoft(int x, int y, double radius) {
         super(x, y, radius);
     }
 
-    public static BlockTile spawnBlockTile(int x, int y) {
+    // Currently 100% spawn rate, to be adjusted at a later time
+    public static BlockSoft spawnBlockSoft(int x, int y) {
         x *= TILE_RADIUS * 2;
         y *= TILE_RADIUS * 2;
-        return new BlockTile(x, y, TILE_RADIUS);
+        return new BlockSoft(x, y, TILE_RADIUS);
     }
 
     @Override
--- src/game/GameMain.java
+++ src/game/GameMain.java
@@ -18,6 +18,30 @@ public class GameMain {
                 if (row % 2 == 1 && column % 2 == 1) {
                     objects.add(BlockHard.spawnBlockHard(row, column));
                 }
+
+                // BlockSoft Row 01
+                if (column == 0 && row >= 2 && row <= 10
+                        || column == 10 && row >= 2 && row <= 10) {
+                    objects.add(BlockSoft.spawnBlockSoft(row, column));
+                }
+
+                // BlockSoft Row 02
+                if (column == 1 && row % 2 == 0 && row != 0 && row != 12
+                        || column == 9 && row % 2 == 0 && row != 0 && row != 12) {
+                    objects.add(BlockSoft.spawnBlockSoft(row, column));
+                }
+
+                // BlockSoft Row 03
+                if (column % 2 == 0 && column != 0 && column != 10) {
+                    objects.add(BlockSoft.spawnBlockSoft(row, column));
+                }
+
+                // BlockSoft Row 04
+                if (column % 2 == 1 && column != 1 && column != 9) {
+                    if (row % 2 == 0) {
+                        objects.add(BlockSoft.spawnBlockSoft(row, column));
+                    }
+                }
             }
         }
     }
