eef72340ddb828fcf889b2d8f6852d49555d01fc
Riaz Philippe <rp16181@essex.ac.uk>
2020-01-25 22:35:51 +0000

    Implemented the correct collision logic for bombs. Bomb's isCollisionActive boolean has been renamed to hasActiveCollision for clarity. GameMain now features some additional checks for dropped bombs that are not being collided with. The Player class has been provided with some code for handling the collisions on bombs with an active collision box. BlockTile's methods have been slightly reordered so that its own static method proceeds its public ones.

--- src/game/Dummy.java
+++ src/game/Dummy.java
@@ -3,7 +3,7 @@ package game;
 public class Dummy {
     /*
     Movement (Needs refinement with rapid movement after placing a bomb/simultaneous key presses)
-    Bomb collision (player)
+    Bomb collision (May need code/logic revision)
     Bomb limit
     Update and refine collision logic
     */
