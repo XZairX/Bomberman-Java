cbef335a7a1968dd81c7ff0bef943ba05685d750
Riaz Philippe <rp16181@essex.ac.uk>
2020-02-01 20:27:12 +0000

    Updated the Dummy notes for the next stage of the project.

--- src/game/Dummy.java
+++ src/game/Dummy.java
@@ -4,5 +4,9 @@ public class Dummy {
     /*
     Collisions still need refinement (holding a direction against a wall)
     Bomb collision (Due for code/logic revision)
+    Refactor listPlayer
+    Fire logic persisting despite hit()
+    Bomb's collisions are incorrect (still emitting detonation Fire some time after being hit)
+    GameView repaint modification bug
     */
 }
